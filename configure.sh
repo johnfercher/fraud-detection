a85314c21030407ce9a25cb212f40691dcd6cd90
John Fercher <johnathanfercher22@gmail.com>
2018-07-29 11:52:52 -0300

    update

--- configure.sh
+++ configure.sh
@@ -4,5 +4,7 @@ sudo apt install jupyter-notebook
 sudo -H pip3 install numpy
 sudo -H pip3 install pandas
 sudo -H pip3 install kaggle
+sudo -H pip3 install matplotlib
 
 kaggle datasets download -d mlg-ulb/creditcardfraud
+kaggle datasets download -d ntnu-testimon/paysim1
