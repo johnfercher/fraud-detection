ede202818ccc425535aead2069205a400003f5ea
John Fercher <johnathanfercher22@gmail.com>
2018-07-29 21:44:21 -0300

    gg easy

--- configure.sh
+++ configure.sh
@@ -5,6 +5,8 @@ sudo -H pip3 install numpy
 sudo -H pip3 install pandas
 sudo -H pip3 install kaggle
 sudo -H pip3 install matplotlib
+sudo -H pip3 install scipy
+sudo -H pip3 install sklearn
 
 kaggle datasets download -d mlg-ulb/creditcardfraud
 kaggle datasets download -d ntnu-testimon/paysim1
